404e843cc45526df9f2eb24828cf881b50f29dd7
Daniil Monin <daniil.monin@outlook.com>
2021-12-17 22:12:53 +0300

    Finishied
    Finishied

---
+++ MyHomeWork/MyHomeWork/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
